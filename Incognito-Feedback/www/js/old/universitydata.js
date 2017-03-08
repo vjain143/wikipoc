918f3637e089c293c805dc8f2f62968765a6c253
admin <Vivek.Jain3@cognizant.com>
2017-03-08 08:03:54 +0000

    fixed search and icon of the app

---
+++ Incognito-Feedback/www/js/old/universitydata.js
@@ -0,0 +1,670 @@
+var universitydata = {"university":
+
+[
+ {
+   "UniversityName": "University of West London",
+   "Town": "London",
+   "Postcode": "W55RF"
+ },
+ {
+   "UniversityName": "University of Westminster",
+   "Town": "London",
+   "Postcode": "W1B2UW"
+ },
+ {
+   "UniversityName": "University of Winchester",
+   "Town": "Winchester",
+   "Postcode": "SO224NR"
+ },
+ {
+   "UniversityName": "University of Wolverhampton",
+   "Town": "Wolverhampton",
+   "Postcode": "WV11LY"
+ },
+ {
+   "UniversityName": "University of Worcester",
+   "Town": "Worcester",
+   "Postcode": "WR26AJ"
+ },
+ {
+   "UniversityName": "University of York",
+   "Town": "York",
+   "Postcode": "YO105DD"
+ },
+ {
+   "UniversityName": "Writtle University College",
+   "Town": "Chelmsford",
+   "Postcode": "CM13RR"
+ },
+ {
+   "UniversityName": "York St John University",
+   "Town": "York",
+   "Postcode": "YO317EX"
+ },
+ {
+   "UniversityName": "University of Kent",
+   "Town": "Canterbury",
+   "Postcode": "CT27NZ"
+ },
+ {
+   "UniversityName": "University of Lancaster",
+   "Town": "Lancaster",
+   "Postcode": "LA14YW"
+ },
+ {
+   "UniversityName": "University of Leeds",
+   "Town": "Leeds",
+   "Postcode": "LS29JT"
+ },
+ {
+   "UniversityName": "University of Leicester",
+   "Town": "Leicester",
+   "Postcode": "LE17RH"
+ },
+ {
+   "UniversityName": "University of Lincoln",
+   "Town": "Lincoln",
+   "Postcode": "LN67TS"
+ },
+ {
+   "UniversityName": "University of Liverpool",
+   "Town": "Liverpool",
+   "Postcode": "L697ZX"
+ },
+ {
+   "UniversityName": "University of London",
+   "Town": "London",
+   "Postcode": "WC1E7HU"
+ },
+ {
+   "UniversityName": "University of Manchester",
+   "Town": "Manchester",
+   "Postcode": "M139PL"
+ },
+ {
+   "UniversityName": "University of Newcastle Upon Tyne",
+   "Town": "Newcastle-upon-Tyne",
+   "Postcode": "NE17RU"
+ },
+ {
+   "UniversityName": "University of Northampton",
+   "Town": "Northampton",
+   "Postcode": "NN27AL"
+ },
+ {
+   "UniversityName": "University of Northumbria At Newcastle",
+   "Town": "Newcastle-upon-Tyne",
+   "Postcode": "NE18ST"
+ },
+ {
+   "UniversityName": "University of Nottingham",
+   "Town": "Nottingham",
+   "Postcode": "NG72RD"
+ },
+ {
+   "UniversityName": "University of Oxford",
+   "Town": "Oxford",
+   "Postcode": "OX12JD"
+ },
+ {
+   "UniversityName": "University of Plymouth",
+   "Town": "Plymouth",
+   "Postcode": "PL48AA"
+ },
+ {
+   "UniversityName": "University of Portsmouth",
+   "Town": "Portsmouth",
+   "Postcode": "PO12UP"
+ },
+ {
+   "UniversityName": "University of Reading",
+   "Town": "Reading",
+   "Postcode": "RG66UR"
+ },
+ {
+   "UniversityName": "University of Salford",
+   "Town": "Salford",
+   "Postcode": "M54WT"
+ },
+ {
+   "UniversityName": "University of Sheffield",
+   "Town": "Sheffield",
+   "Postcode": "S102TN"
+ },
+ {
+   "UniversityName": "University of Southampton",
+   "Town": "Southampton",
+   "Postcode": "SO171BJ"
+ },
+ {
+   "UniversityName": "University of Sunderland",
+   "Town": "Sunderland",
+   "Postcode": "SR13SD"
+ },
+ {
+   "UniversityName": "University of Surrey",
+   "Town": "Guildford",
+   "Postcode": "GU27XH"
+ },
+ {
+   "UniversityName": "University of Sussex",
+   "Town": "Brighton",
+   "Postcode": "BN19RH"
+ },
+ {
+   "UniversityName": "University of the Arts London",
+   "Town": "London",
+   "Postcode": "WC1V7EY"
+ },
+ {
+   "UniversityName": "University of the West of England, Bristol",
+   "Town": "Bristol",
+   "Postcode": "BS161QY"
+ },
+ {
+   "UniversityName": "University of Warwick",
+   "Town": "Coventry",
+   "Postcode": "CV48UW"
+ },
+ {
+   "UniversityName": "University of Bath",
+   "Town": "Bath",
+   "Postcode": "BA27AY"
+ },
+ {
+   "UniversityName": "University of Bedfordshire",
+   "Town": "Luton",
+   "Postcode": "LU13JU"
+ },
+ {
+   "UniversityName": "University of Birmingham",
+   "Town": "Birmingham",
+   "Postcode": "B152TT"
+ },
+ {
+   "UniversityName": "University of Bolton",
+   "Town": "Bolton",
+   "Postcode": "BL35AB"
+ },
+ {
+   "UniversityName": "University of Bradford",
+   "Town": "Bradford",
+   "Postcode": "BD71DP"
+ },
+ {
+   "UniversityName": "University of Brighton",
+   "Town": "Brighton",
+   "Postcode": "BN24AT"
+ },
+ {
+   "UniversityName": "University of Bristol",
+   "Town": "Bristol",
+   "Postcode": "BS81TH"
+ },
+ {
+   "UniversityName": "University of Buckingham",
+   "Town": "Buckingham",
+   "Postcode": "MK181EG"
+ },
+ {
+   "UniversityName": "University of Cambridge",
+   "Town": "Cambridge",
+   "Postcode": "CB21TN"
+ },
+ {
+   "UniversityName": "University of Central Lancashire",
+   "Town": "Preston",
+   "Postcode": "PR12HE"
+ },
+ {
+   "UniversityName": "University of Chester",
+   "Town": "Chester",
+   "Postcode": "CH14BJ"
+ },
+ {
+   "UniversityName": "University of Chichester",
+   "Town": "Chichester",
+   "Postcode": "PO196PE"
+ },
+ {
+   "UniversityName": "University of Cumbria",
+   "Town": "Carlisle",
+   "Postcode": "CA38TB"
+ },
+ {
+   "UniversityName": "University of Derby",
+   "Town": "Derby",
+   "Postcode": "DE221GB"
+ },
+ {
+   "UniversityName": "University of Durham",
+   "Town": "Durham",
+   "Postcode": "DH13HP"
+ },
+ {
+   "UniversityName": "University of East Anglia",
+   "Town": "Norwich",
+   "Postcode": "NR47TJ"
+ },
+ {
+   "UniversityName": "University of East London",
+   "Town": "London",
+   "Postcode": "E162RD"
+ },
+ {
+   "UniversityName": "University of Essex",
+   "Town": "Colchester",
+   "Postcode": "CO43SQ"
+ },
+ {
+   "UniversityName": "University of Exeter",
+   "Town": "Exeter",
+   "Postcode": "EX44QJ"
+ },
+ {
+   "UniversityName": "University of Gloucestershire",
+   "Town": "Cheltenham",
+   "Postcode": "GL502RH"
+ },
+ {
+   "UniversityName": "University of Greenwich",
+   "Town": "London",
+   "Postcode": "SE109LS"
+ },
+ {
+   "UniversityName": "University of Hertfordshire",
+   "Town": "Hatfield",
+   "Postcode": "AL109AB"
+ },
+ {
+   "UniversityName": "University of Huddersfield",
+   "Town": "Huddersfield",
+   "Postcode": "HD13DH"
+ },
+ {
+   "UniversityName": "University of Hull",
+   "Town": "Kingston-upon-Hull",
+   "Postcode": "HU67RX"
+ },
+ {
+   "UniversityName": "University of Keele",
+   "Town": "Keele",
+   "Postcode": "ST55BG"
+ },
+ {
+   "UniversityName": "Oxford Brookes University",
+   "Town": "Oxford",
+   "Postcode": "OX30BP"
+ },
+ {
+   "UniversityName": "Plymouth College of Art",
+   "Town": "Plymouth",
+   "Postcode": "PL48AT"
+ },
+ {
+   "UniversityName": "Queen Mary and Westfield College",
+   "Town": "London",
+   "Postcode": "E14NS"
+ },
+ {
+   "UniversityName": "Ravensbourne",
+   "Town": "London",
+   "Postcode": "SE100EW"
+ },
+ {
+   "UniversityName": "Roehampton University",
+   "Town": "London",
+   "Postcode": "SW155PJ"
+ },
+ {
+   "UniversityName": "Rose Bruford College",
+   "Town": "Sidcup",
+   "Postcode": "DA159DF"
+ },
+ {
+   "UniversityName": "Royal Academy of Music",
+   "Town": "London",
+   "Postcode": "NW15HT"
+ },
+ {
+   "UniversityName": "Royal Agricultural University",
+   "Town": "Cirencester",
+   "Postcode": "GL76JS"
+ },
+ {
+   "UniversityName": "Royal College of Art",
+   "Town": "London",
+   "Postcode": "SW72EU"
+ },
+ {
+   "UniversityName": "Royal College of Music",
+   "Town": "London",
+   "Postcode": "SW72BS"
+ },
+ {
+   "UniversityName": "The Royal College of Nursing",
+   "Town": "London",
+   "Postcode": "W1G0RN"
+ },
+ {
+   "UniversityName": "Royal Holloway and Bedford New College",
+   "Town": "Egham",
+   "Postcode": "TW200EX"
+ },
+ {
+   "UniversityName": "Royal Northern College of Music",
+   "Town": "Manchester",
+   "Postcode": "M139RD"
+ },
+ {
+   "UniversityName": "The Royal Veterinary College",
+   "Town": "London",
+   "Postcode": "NW10TU"
+ },
+ {
+   "UniversityName": "St George's Hospital Medical School",
+   "Town": "London",
+   "Postcode": "SW170RE"
+ },
+ {
+   "UniversityName": "St Mary's University, Twickenham",
+   "Town": "Twickenham",
+   "Postcode": "TW14SX"
+ },
+ {
+   "UniversityName": "School of Oriental and African Studies",
+   "Town": "London",
+   "Postcode": "WC1H0XG"
+ },
+ {
+   "UniversityName": "Sheffield Hallam University",
+   "Town": "Sheffield",
+   "Postcode": "S11WB"
+ },
+ {
+   "UniversityName": "Southampton Solent University",
+   "Town": "Southampton",
+   "Postcode": "SO140YN"
+ },
+ {
+   "UniversityName": "Staffordshire University",
+   "Town": "Stafford",
+   "Postcode": "ST180AD"
+ },
+ {
+   "UniversityName": "Teesside University",
+   "Town": "Middlesbrough",
+   "Postcode": "TS13BA"
+ },
+ {
+   "UniversityName": "Trinity Laban",
+   "Town": "Deptford",
+   "Postcode": "SE83DZ"
+ },
+ {
+   "UniversityName": "University College Birmingham",
+   "Town": "Birmingham",
+   "Postcode": "B31JB"
+ },
+ {
+   "UniversityName": "University College for the Creative Arts",
+   "Town": "Farnham",
+   "Postcode": "GU97DS"
+ },
+ {
+   "UniversityName": "University College London",
+   "Town": "London",
+   "Postcode": "WC1E6BT"
+ },
+ {
+   "UniversityName": "Imperial College of Science, Technology and Medicine",
+   "Town": "London",
+   "Postcode": "SW72AZ"
+ },
+ {
+   "UniversityName": "Institute of Cancer Research",
+   "Town": "London",
+   "Postcode": "SW36JB"
+ },
+ {
+   "UniversityName": "Institute of Education",
+   "Town": "London",
+   "Postcode": "WC1H0AL"
+ },
+ {
+   "UniversityName": "King's College London",
+   "Town": "London",
+   "Postcode": "SE18WA"
+ },
+ {
+   "UniversityName": "Kingston University",
+   "Town": "Kingston upon Thames",
+   "Postcode": "KT11LQ"
+ },
+ {
+   "UniversityName": "Leeds Beckett University",
+   "Town": "Leeds",
+   "Postcode": "LS63QS"
+ },
+ {
+   "UniversityName": "Leeds College of Art",
+   "Town": "Leeds",
+   "Postcode": "LS29AQ"
+ },
+ {
+   "UniversityName": "Leeds College of Music",
+   "Town": "Leeds",
+   "Postcode": "LS27PD"
+ },
+ {
+   "UniversityName": "Leeds Trinity University",
+   "Town": "Leeds",
+   "Postcode": "LS185HD"
+ },
+ {
+   "UniversityName": "Liverpool Hope University",
+   "Town": "Liverpool",
+   "Postcode": "L169JD"
+ },
+ {
+   "UniversityName": "Liverpool Institute of Performing Arts",
+   "Town": "Liverpool",
+   "Postcode": "L19HF"
+ },
+ {
+   "UniversityName": "Liverpool John Moores University",
+   "Town": "Liverpool",
+   "Postcode": "L35UX"
+ },
+ {
+   "UniversityName": "London Business School",
+   "Town": "London",
+   "Postcode": "NW14SA"
+ },
+ {
+   "UniversityName": "London Metropolitan University",
+   "Town": "London",
+   "Postcode": "N78DB"
+ },
+ {
+   "UniversityName": "London School of Economics and Political Science",
+   "Town": "London",
+   "Postcode": "WC2A2AE"
+ },
+ {
+   "UniversityName": "London School of Hygiene & Tropical Medicine",
+   "Town": "London",
+   "Postcode": "WC1E7HT"
+ },
+ {
+   "UniversityName": "London South Bank University",
+   "Town": "London",
+   "Postcode": "SE10AA"
+ },
+ {
+   "UniversityName": "Loughborough University",
+   "Town": "Loughborough",
+   "Postcode": "LE113TU"
+ },
+ {
+   "UniversityName": "The Manchester Metropolitan University",
+   "Town": "Manchester",
+   "Postcode": "M156BH"
+ },
+ {
+   "UniversityName": "Middlesex University",
+   "Town": "London",
+   "Postcode": "N144YZ"
+ },
+ {
+   "UniversityName": "Newman University College",
+   "Town": "Birmingham",
+   "Postcode": "B323NT"
+ },
+ {
+   "UniversityName": "Northern School of Contemporary Dance",
+   "Town": "Leeds",
+   "Postcode": "LS74BH"
+ },
+ {
+   "UniversityName": "Norwich University of the Arts",
+   "Town": "Norwich",
+   "Postcode": "NR24SN"
+ },
+ {
+   "UniversityName": "The Nottingham Trent University",
+   "Town": "Nottingham",
+   "Postcode": "NG14BU"
+ },
+ {
+   "UniversityName": "The Open University",
+   "Town": "Milton Keynes",
+   "Postcode": "MK76AA"
+ },
+ {
+   "UniversityName": "Anglia Ruskin University",
+   "Town": "Chelmsford",
+   "Postcode": "CM11SQ"
+ },
+ {
+   "UniversityName": "The Arts University College At Bournemouth",
+   "Town": "Poole",
+   "Postcode": "BH125HH"
+ },
+ {
+   "UniversityName": "Aston University",
+   "Town": "Birmingham",
+   "Postcode": "B47ET"
+ },
+ {
+   "UniversityName": "Bath Spa University",
+   "Town": "Bath",
+   "Postcode": "BA29BN"
+ },
+ {
+   "UniversityName": "Birkbeck College",
+   "Town": "London",
+   "Postcode": "WC1E7HX"
+ },
+ {
+   "UniversityName": "Birmingham City University",
+   "Town": "Birmingham",
+   "Postcode": "B422SU"
+ },
+ {
+   "UniversityName": "Bishop Grosseteste University College Lincoln",
+   "Town": "Lincoln",
+   "Postcode": "LN13DY"
+ },
+ {
+   "UniversityName": "Bournemouth University",
+   "Town": "Poole",
+   "Postcode": "BH125BB"
+ },
+ {
+   "UniversityName": "Brunel University",
+   "Town": "Uxbridge",
+   "Postcode": "UB83PH"
+ },
+ {
+   "UniversityName": "Buckinghamshire New University",
+   "Town": "High Wycombe",
+   "Postcode": "HP112JZ"
+ },
+ {
+   "UniversityName": "Canterbury Christ Church University",
+   "Town": "Canterbury",
+   "Postcode": "CT11QU"
+ },
+ {
+   "UniversityName": "Central School of Speech and Drama",
+   "Town": "London",
+   "Postcode": "NW33HY"
+ },
+ {
+   "UniversityName": "City University",
+   "Town": "London",
+   "Postcode": "EC1V0HB"
+ },
+ {
+   "UniversityName": "College of St Mark & St John",
+   "Town": "Plymouth",
+   "Postcode": "PL68BH"
+ },
+ {
+   "UniversityName": "Conservatoire for Dance and Drama",
+   "Town": "London",
+   "Postcode": "WC1H0JJ"
+ },
+ {
+   "UniversityName": "Courtauld Institute of Art",
+   "Town": "London",
+   "Postcode": "WC2R0RN"
+ },
+ {
+   "UniversityName": "Coventry University",
+   "Town": "Coventry",
+   "Postcode": "CV15FB"
+ },
+ {
+   "UniversityName": "Cranfield University",
+   "Town": "Cranfield",
+   "Postcode": "MK430AL"
+ },
+ {
+   "UniversityName": "De Montfort University",
+   "Town": "Leicester",
+   "Postcode": "LE19BH"
+ },
+ {
+   "UniversityName": "Edge Hill University",
+   "Town": "Ormskirk",
+   "Postcode": "L394QP"
+ },
+ {
+   "UniversityName": "Falmouth University",
+   "Town": "Falmouth",
+   "Postcode": "TR114RH"
+ },
+ {
+   "UniversityName": "Goldsmiths College, University of London",
+   "Town": "London",
+   "Postcode": "SE146NW"
+ },
+ {
+   "UniversityName": "Guildhall School of Music and Drama",
+   "Town": "London",
+   "Postcode": "EC2Y8DT"
+ },
+ {
+   "UniversityName": "Harper Adams University",
+   "Town": "Newport",
+   "Postcode": "TF108NB"
+ },
+ {
+   "UniversityName": "Heythrop College",
+   "Town": "London",
+   "Postcode": "W85HQ"
+ }
+]
+};

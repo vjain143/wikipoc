32ddeddec9cca77b30d275008fee5f72270e8a58
Vivek Jain <5896522+vjain143@users.noreply.github.com>
2023-07-26 14:09:19 +0100

    Update drop.view.sql

--- docs/drop.view.sql
+++ docs/drop.view.sql
@@ -1,11 +1,12 @@
 -- Replace 'your_schema_name' with the name of the schema containing the tables you want to drop
-DO $$ 
 DECLARE 
-  table_name text;
+  table_name varchar;
+  query varchar;
 BEGIN 
   FOR table_name IN (SELECT table_name FROM information_schema.tables WHERE table_schema = 'your_schema_name') 
   LOOP
-    EXECUTE 'DROP TABLE IF EXISTS ' || table_name || ' CASCADE';
-    RAISE NOTICE 'Dropped table: %', table_name;
+    SET query = 'DROP TABLE IF EXISTS ' || table_name || ' CASCADE';
+    EXECUTE IMMEDIATE query;
+    SELECT 'Dropped table: ' || table_name;
   END LOOP;
-END $$;
+END;
